165b875a13471071361c6525cce16ce73993f7b4
Andrew Woodruff <agwoodruff93@gmail.com>
2021-12-04 10:01:43 -0700

    day 2 finished, and refactor

--- 2021/Day1/day1.exs
+++ 2021/Day1/day1.exs
@@ -1,12 +1,29 @@
-tuple = File.read!("input.txt")
-        |> String.split("\n", trim: true)
-        |> Enum.map(fn string -> String.to_integer(string) end)
-        |> Enum.reduce({ :whatever, 0 }, fn num, { prev, acc } ->
-             if num > prev do
-               { num, acc + 1 }
-             else
-               { num, acc }
-             end
-           end)
+defmodule Day1 do
+  def result(data) do
+    result = Enum.reduce(data, { :whatever, 0 }, fn num, { prev, acc } ->
+      if num > prev do
+        { num, acc + 1 }
+      else
+        { num, acc }
+      end
+    end)
 
-IO.puts(elem(tuple, 1))
+    Integer.to_string(elem(result, 1))
+  end
+end
+
+formatted_data = File.read!("input.txt")
+                 |> String.split("\n", trim: true)
+                 |> Enum.map(fn string -> String.to_integer(string) end)
+
+# PART 1
+result_part_one = Day1.result(formatted_data)
+
+# PART 2
+rolling_data = Enum.chunk_every(formatted_data, 3, 1, :discard)
+               |> Enum.map(fn [a, b, c] -> a + b + c end)
+
+result_part_two = Day1.result(rolling_data)
+
+IO.puts("PART 1 RESULT: " <> result_part_one)
+IO.puts("PART 2 RESULT: " <> result_part_two)
